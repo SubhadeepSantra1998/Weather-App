9b0df947972a9607de4beb945c311198658f31b7
Subha <androdeep25@gmail.com>
2023-01-11 14:21:40 +0530

    Final Commit

---
+++ app/src/main/java/com/example/myweatherapp/presentation/ui/theme/Theme.kt
@@ -0,0 +1,14 @@
+package com.example.myweatherapp.presentation.ui.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MyWeatherAppTheme(content: @Composable () -> Unit) {
+
+    MaterialTheme(
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
